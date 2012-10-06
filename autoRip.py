2379ac03d708c5a4651c2ae2c2755f76a5d4edf5
Jason <jason@jcode.me>
2012-10-06 17:41:49 +0930

    More comments

--- autoRip.py
+++ autoRip.py
@@ -29,9 +29,26 @@
 # @version    $Id$
 # @author     Jason Millward <jason@jcode.me>
 # @license    http://opensource.org/licenses/MIT
+
+
+#
+#   CONSTANTS
 #
 
-# Import libs
+# Path where the movies are stored
+MKV_SAVE_PATH = "/Movies/"
+
+# Minimum Length of video stream in minutes
+MKV_MIN_LENGTH = 4800
+
+# MakeMKV Cache
+MKV_CACHE = 1024
+
+
+#
+#   IMPORTS
+#
+
 import commands
 import os.path
 import datetime
@@ -39,31 +56,34 @@ import imdb
 import sys
 
 #
-MKV_SAVE_PATH = "/Movies/"
+#   CODE
 #
-MKV_MIN_LENGTH = 4800
-#
-MKV_CACHE = 1024
-
-i = imdb.IMDb()
 
 # Setup some vars
+imdbScaper = imdb.IMDb()
 discIndex = ""
 movieName = ""
 
 # Execute the info gathering
+# Save output into /tmp/ for interpreting 3 or 4 lines later
 commands.getstatusoutput('makemkvcon -r info > /tmp/makemkv_output')
 
-# Open the output file
-f = open('/tmp/makemkv_output', 'r')
+# Open the info file from /tmp/
+tempFile = open('/tmp/makemkv_output', 'r')
 
 # Check to see if there is a disc in the system
-for line in f.readlines():
+#
+# For every line in the output
+# If the first 4 characters are DRV:
+#   Check to see if a device is specified
+#       If so, get the 1st and 6th element of the array
+for line in tempFile.readlines():
     if line[:4] == "DRV:":
         if "/dev/" in line:
             drive = line.split(',')
-            movieName = drive[5].replace("\"", "").title().replace("Extended_Edition", "")
             discIndex = drive[0].replace("DRV:", "")
+            movieName = drive[5].title().replace("Extended_Edition", "")
+
 
 
 # If there was no disc, exit
@@ -71,9 +91,9 @@ if len(discIndex) == 0:
     print "No disc"
     sys.exit()
 
-movieName = movieName.replace("_", " ")
+movieName = movieName.replace("\"", "").replace("_", " ")
 
-sm = i.search_movie(movieName, results=5)
+sm = imdbScaper.search_movie(movieName, results=5)
 
 
 if len(sm) > 0:
