ce430e466d6f5aa3ddc23d34b4d189ad4021816e
JasonMillward <jason@jcode.me>
2013-01-23 07:47:53 +1030

    Moving files around (refactoring)

---
+++ makemkv/__init__.py
@@ -0,0 +1,3 @@
+#!/usr/bin/python
+
+from .makeMKV import makeMKV
---
+++ timer/timer.py
@@ -0,0 +1,40 @@
+"""
+MakeMKV CLI Wrapper
+
+This class acts as a python wrapper to the MakeMKV CLI.
+
+
+Released under the MIT license
+Copyright (c) 2012, Jason Millward
+
+@category   misc
+@version    $Id: 1.2, 2013-01-20 11:08:00 CST $;
+@author     Jason Millward <jason@jcode.me>
+@license    http://opensource.org/licenses/MIT
+"""
+
+#
+#   IMPORTS
+#
+
+import datetime
+
+#
+#   CODE
+#
+
+
+class makeMKV(object):
+
+    def __init__():
+        startTime = datetime.datetime.now()
+
+    def stop():
+        endTime = datetime.datetime.now()
+
+        totalTime = endTime - startTime
+
+        minutes = totalTime.seconds / 60
+
+    def getTime():
+        return minutes
