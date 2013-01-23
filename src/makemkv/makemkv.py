b72931ed658c7bb4bf6938f1bc810ceb22f85976
JasonMillward <jason@jcode.me>
2013-01-23 11:39:41 +1030

    Added docco to makemkv.py

--- src/makemkv/makemkv.py
+++ src/makemkv/makemkv.py
@@ -30,27 +30,27 @@ import re
 class makeMKV(object):
 
     """ Function:   __init__
-            Removes the log file and the input movie because these files are
-                no longer needed by this script
+            Initialises the variables that will be used in this class
 
         Inputs:
-            log         (Str): File path of the log to remove
-            oldMovie    (Str): File path of the movie to remove
+            None
 
         Outputs:
             None
     """
     def __init__(self):
+        self.discIndex = 0
+        self.movieName = ""
+        self.path = ""
         self.movieName = ""
         self.imdbScaper = imdb.IMDb()
 
     """ Function:   _queueMovie
-            Removes the log file and the input movie because these files are
-                no longer needed by this script
+            Adds the recently ripped movie to the queue file for the compress
+                script to handle later on
 
         Inputs:
-            log         (Str): File path of the log to remove
-            oldMovie    (Str): File path of the movie to remove
+            None
 
         Outputs:
             None
@@ -72,12 +72,10 @@ class makeMKV(object):
                 (self.path, self.movieName, movie, self.movieName))
 
     """ Function:   _cleanTitle
-            Removes the log file and the input movie because these files are
-                no longer needed by this script
+            Removes the extra bits in the title and removes whitespace
 
         Inputs:
-            log         (Str): File path of the log to remove
-            oldMovie    (Str): File path of the movie to remove
+            None
 
         Outputs:
             None
@@ -100,44 +98,59 @@ class makeMKV(object):
         self.movieName = tmpName.strip()
 
     """ Function:   ripDisc
-            Removes the log file and the input movie because these files are
-                no longer needed by this script
+            Passes in all of the arguments to makemkvcon to start the ripping
+                of the currently inserted DVD or BD
 
         Inputs:
-            log         (Str): File path of the log to remove
-            oldMovie    (Str): File path of the movie to remove
+            path    (Str):  Where the movie will be saved to
+            length  (Int):  Minimum length of the main movie
+            cache   (Int):  Cache in MB
+            queue   (Bool): Save movie into queue for compressing later
+            output  (Str):  Temp file to save output to
 
         Outputs:
-            None
+            Success (Bool)
     """
-    def ripDisc(self, path, length, cache, queue):
+    def ripDisc(self, path, length, cache, queue, output):
         self.path = path
-        # Start the making of the mkv
-        commands.getstatusoutput(
-            'makemkvcon mkv disc:%s 0 "%s/%s" --cache=%d --noscan --minlength=%d'
-            %
-            (self.discIndex, self.path, self.movieName, cache, length))
 
-        # Add some checking here
-        # Seriously, check to see if makemkv worked like it should :(
-        if queue:
-            self._queueMovie()
-        return True
+        args = (" --cache=%d --noscan --minlength=%d > %s"
+            % (cache, length, output))
+
+        commands.getstatusoutput(
+            'makemkvcon mkv disc:%s 0 "%s/%s" %s'
+            %
+            (self.discIndex, self.path, self.movieName, args))
+
+        checks = 0
+        try:
+            tempFile = open(output, 'r')
+            for line in tempFile.readlines():
+                if "Copy complete" in line:
+                    checks += 1
+                if "titles saved" in line:
+                    checks += 1
+        except:
+            print "Could not read output file"
+
+        if checks >= 2:
+            if queue:
+                self._queueMovie()
+            return True
+        else:
+            return False
 
     """ Function:   findDisc
-            Removes the log file and the input movie because these files are
-                no longer needed by this script
+            Use makemkvcon to list all DVDs or BDs inserted
+            If more then one disc is inserted, use the first result
 
         Inputs:
-            log         (Str): File path of the log to remove
-            oldMovie    (Str): File path of the movie to remove
+            output  (Str): Temp file to save output to
 
         Outputs:
-            None
+            Success (Bool)
     """
     def findDisc(self, output):
-        # Execute the info gathering
-        # Save output into /tmp/ for interpreting 3 or 4 lines later
         commands.getstatusoutput('makemkvcon -r info > %s' % output)
 
         # Open the info file from /tmp/
@@ -156,15 +169,13 @@ class makeMKV(object):
             return True
 
     """ Function:   getTitle
-            Removes the log file and the input movie because these files are
-                no longer needed by this script
+            Returns the current movies title
 
         Inputs:
-            log         (Str): File path of the log to remove
-            oldMovie    (Str): File path of the movie to remove
+            None
 
         Outputs:
-            None
+            movieName   (Str)
     """
     def getTitle(self):
         self._cleanTitle()
