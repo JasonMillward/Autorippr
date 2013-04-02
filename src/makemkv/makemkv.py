50ef5ecc1f8222e959d2289f3dfe59e292a9a306
JasonMillward <jason@jcode.me>
2013-04-03 09:45:29 +1030

    Bumped version to 1.4, support for python 2.7.2

--- src/makemkv/makemkv.py
+++ src/makemkv/makemkv.py
@@ -8,7 +8,7 @@ Released under the MIT license
 Copyright (c) 2012, Jason Millward
 
 @category   misc
-@version    $Id: 1.3, 2013-02-11 09:27:38 CST $;
+@version    $Id: 1.4, 2013-04-03 09:41:53 CST $;
 @author     Jason Millward <jason@jcode.me>
 @license    http://opensource.org/licenses/MIT
 """
