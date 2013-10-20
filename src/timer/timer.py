60f0f1c5abff57e2e044c483649daa776f0a4166
Jason <jason@jcode.me>
2013-10-20 20:58:55 +1030

    Bumping version yet again :(

--- src/timer/timer.py
+++ src/timer/timer.py
@@ -8,7 +8,7 @@ Released under the MIT license
 Copyright (c) 2012, Jason Millward
 
 @category   misc
-@version    $Id: 1.4, 2013-04-03 09:41:53 CST $;
+@version    $Id: 1.5, 2013-10-20 20:40:30 CST $;
 @author     Jason Millward <jason@jcode.me>
 @license    http://opensource.org/licenses/MIT
 """
