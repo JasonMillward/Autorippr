12249df701fb4371f8eeda9f4b0e81ceaca673fb
Jason Millward <jason@jcode.me>
2014-03-20 13:32:58 +1030

    logger = logger.logger

--- classes/handbrake.py
+++ classes/handbrake.py
@@ -23,7 +23,7 @@ class handBrake(object):
 
     def __init__(self, debugLevel):
         self.db = database.database()
-        self.log = logger("handbrake", debugLevel)
+        self.log = logger.logger("handbrake", debugLevel)
 
     """ Function:   _cleanUp
             Removes the log file and the input movie because these files are
