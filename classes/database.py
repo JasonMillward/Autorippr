9855de22b7ee029aa9f74f402f43bf71719d0db9
Jason Millward <jason@jcode.me>
2014-04-29 16:05:53 +0930

    Giving peewee a try

--- classes/database.py
+++ classes/database.py
@@ -1,5 +1,5 @@
 """
-MakeMKV CLI Wrapper
+SQLite Database Helper
 
 
 Released under the MIT license
@@ -12,78 +12,5 @@ Copyright (c) 2012, Jason Millward
 """
 
 import os
-import sqlite3
-
-class database(object):
-
-    def __init__(self):
-        DATABASE = 'autoripper.db'
-        REAL_PATH = os.path.dirname(os.path.realpath(__file__))
-
-        self.con = sqlite3.connect('%s/../%s' % (REAL_PATH, DATABASE))
-
-        if not self._tableExists():
-            self._createStructure()
-
-    def _tableExists(self):
-        with self.con:
-
-            cur = self.con.cursor()
-            uSql = ("SELECT name ",
-                    "FROM   sqlite_master ",
-                    "WHERE  type='table' ",
-                    "AND    name='movies' ",
-                    "LIMIT 1;")
-
-            cur.execute(''.join(uSql))
-            data = cur.fetchone()
-
-            if isinstance(data, tuple) and ''.join(data) == "movies":
-                return True
-            else:
-                return False
-
-    def _createStructure(self):
-        with self.con:
-            cur = self.con.cursor()
-            uSql = ("CREATE TABLE movies ("
-                    "ID         INTEGER PRIMARY KEY AUTOINCREMENT, ",
-                    "path       TEXT, ",
-                    "inMovie    TEXT, ",
-                    "outMovie   TEXT, ",
-                    "status     TEXT, ",
-                    "statusText TEXT)")
-
-            cur.execute(''.join(uSql))
-
-    def insert(self, path, inMovie, outMovie):
-        with self.con:
-            cur = self.con.cursor()
-            uSql = ("INSERT INTO movies ",
-                    "(path, inMovie, outMovie, status, statusText) ",
-                    "VALUES ('%s', '%s', '%s', 'In Queue', 'Waiting');"
-                    %
-                    (path, inMovie.replace("'","''"), outMovie.replace("'","''"))
-            )
-            cur.execute(''.join(uSql))
-
-    def update(self, uid, status, text):
-        with self.con:
-            cur = self.con.cursor()
-            uSql = ("UPDATE  movies ",
-                   "SET     status=?, ",
-                   "        statusText=? ",
-                   "WHERE   ID=?")
-
-            cur.execute(''.join(uSql), (status, text, uid))
-            self.con.commit()
-
-    def getNextMovie(self):
-        with self.con:
-            cur = self.con.cursor()
-            uSql = ("SELECT ID, path, inMovie, outMovie ",
-                   "FROM movies WHERE status = 'In Queue'")
-            cur.execute(''.join(uSql))
-
-            return cur.fetchone()
+from peewee import database
 
