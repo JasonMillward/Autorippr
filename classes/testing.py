27a62b561cef3ec8f459f0b1b537a849ee5f9bb3
Jason Millward <jason@jcode.me>
2014-07-25 21:15:30 +0930

    Adding testing class

---
+++ classes/testing.py
@@ -0,0 +1,60 @@
+"""
+Configuration and requirements testing
+
+
+Released under the MIT license
+Copyright (c) 2012, Jason Millward
+
+@category   misc
+@version    $Id: 1.6, 2014-07-21 18:48:00 CST $;
+@author     Jason Millward <jason@jcode.me>
+@license    http://opensource.org/licenses/MIT
+"""
+
+import sys
+import os
+import subprocess
+
+def perform_testing(config):
+
+    requirements = {
+        "MakeMKV":   "makemkvcon",
+        "Filebot":   "filebot",
+        "HandBrake": "HandBrakeCLI"
+    }
+
+    print "= Checking directory permissions"
+    print canWrite( config['makemkv']['savePath'] ), "makemkv savePath"
+
+    print ""
+    print "= Checking requirements"
+    for req in requirements:
+        print checkCommand(requirements[req]), req
+
+    sys.exit(0)
+
+def canWrite(path):
+    try:
+        ret = boolToStatus( os.access(path, os.W_OK | os.X_OK) )
+    except:
+        ret = False
+    finally:
+        return ret
+
+def boolToStatus(inBool):
+    if inBool:
+        return "[  OK  ]"
+    else:
+        return "[ FAIL ]"
+
+def checkCommand(com):
+    proc = subprocess.Popen(
+        [
+            'which',
+            str(com)
+        ],
+        stderr=subprocess.PIPE,
+        stdout=subprocess.PIPE
+    )
+    return boolToStatus( len(proc.stdout.read()) > 0 )
+
